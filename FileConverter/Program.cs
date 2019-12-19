6245ed5cad114eca7ec4a636c5b8ca861c032fac
Alexander Krahl <alexander.krahl@stud.htwk-leipzig.de>
2019-12-19 12:12:49 +0100

    fix converter

--- FileConverter/Program.cs
+++ FileConverter/Program.cs
@@ -12,25 +12,27 @@ namespace FileConverter
 
             for (int i = 0; i < files.Length; i++)
             {
-                if (!files[i].Contains("temperature") || files[i].Contains("pressure")) continue;
-                string[] text = File.ReadAllLines(files[i]);
-                DateTime[] dateTimes = new DateTime[text.Length];
-                string[] data = new string[text.Length];
-                for (int j = 0; j < text.Length; j++)
+                if ((files[i].Contains("temperature") || files[i].Contains("pressure")) && files[i].Contains(".csv"))
                 {
-                    string[] splits = text[j].Split(';');
-                    dateTimes[j] = DateTime.Parse(splits[0]);
-                    data[j] = splits[1];
+                    string[] text = File.ReadAllLines(files[i]);
+                    DateTime[] dateTimes = new DateTime[text.Length];
+                    string[] data = new string[text.Length];
+                    for (int j = 0; j < text.Length; j++)
+                    {
+                        string[] splits = text[j].Split(';');
+                        dateTimes[j] = DateTime.ParseExact(splits[0], "MM-dd-yyyy HH:mm:ss", null);
+                        data[j] = splits[1];
+                    }
+                    string json = JsonConvert.SerializeObject(dateTimes);
+                    string json2 = JsonConvert.SerializeObject(data);
+                    json = json.Substring(1);
+                    json2 = json2.Substring(1);
+                    int lastDirSep = files[i].LastIndexOf(Path.DirectorySeparatorChar);
+                    int length = files[i].Length - 5 - lastDirSep;
+                    json = "[\"\"," + json;
+                    json2 = "[\"" + files[i].Substring(lastDirSep + 1, length) + "\"," + json2;
+                    File.WriteAllText(files[i].Substring(0, files[i].Length - 4) + ".json", "{ \"dates\": " + json + ", \"vals\": " + json2 + "}");
                 }
-                string json = JsonConvert.SerializeObject(dateTimes);
-                string json2 = JsonConvert.SerializeObject(data);
-                json = json.Substring(1);
-                json2 = json2.Substring(1);
-                int lastDirSep = files[i].LastIndexOf(Path.DirectorySeparatorChar);
-                int length = files[i].Length - 5 - lastDirSep;
-                json = "[\"\"," + json;
-                json2 = "[\"" + files[i].Substring(lastDirSep + 1, length) + "\"," + json2;
-                File.WriteAllText(files[i].Substring(0, files[i].Length - 4) + ".json", "{ \"dates\": " + json + ", \"vals\": " + json2 + "}");
             }
         }
     }
