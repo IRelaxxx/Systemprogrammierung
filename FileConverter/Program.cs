df7d244289f09acf00077ef9120e0fb65ac1f6fe
Alexander Krahl <alexander.krahl@stud.htwk-leipzig.de>
2020-01-06 13:24:23 +0100

    fileconverter now only works on single files

--- FileConverter/Program.cs
+++ FileConverter/Program.cs
@@ -8,32 +8,30 @@ namespace FileConverter
     {
         private static void Main(string[] args)
         {
-            string[] files = Directory.GetFiles(args.Length > 2 ? args[1] : Directory.GetCurrentDirectory());
-
-            for (int i = 0; i < files.Length; i++)
+            string file = args.Length > 2 ? args[1] : string.Empty;
+            if (string.IsNullOrEmpty(file))
             {
-                if ((files[i].Contains("temperature") || files[i].Contains("pressure")) && files[i].Contains(".csv"))
-                {
-                    string[] text = File.ReadAllLines(files[i]);
-                    DateTime[] dateTimes = new DateTime[text.Length];
-                    string[] data = new string[text.Length];
-                    for (int j = 0; j < text.Length; j++)
-                    {
-                        string[] splits = text[j].Split(';');
-                        dateTimes[j] = DateTime.ParseExact(splits[0], "MM-dd-yyyy HH:mm:ss", null);
-                        data[j] = splits[1];
-                    }
-                    string json = JsonConvert.SerializeObject(dateTimes);
-                    string json2 = JsonConvert.SerializeObject(data);
-                    json = json.Substring(1);
-                    json2 = json2.Substring(1);
-                    int lastDirSep = files[i].LastIndexOf(Path.DirectorySeparatorChar);
-                    int length = files[i].Length - 5 - lastDirSep;
-                    json = "[\"\"," + json;
-                    json2 = "[\"" + files[i].Substring(lastDirSep + 1, length) + "\"," + json2;
-                    File.WriteAllText(files[i].Substring(0, files[i].Length - 4) + ".json", "{ \"dates\": " + json + ", \"vals\": " + json2 + "}");
-                }
+                Console.WriteLine("Usage: fileconverter FILEPATH");
             }
+
+            string[] text = File.ReadAllLines(file);
+            DateTime[] dateTimes = new DateTime[text.Length];
+            string[] data = new string[text.Length];
+            for (int j = 0; j < text.Length; j++)
+            {
+                string[] splits = text[j].Split(';');
+                dateTimes[j] = DateTime.ParseExact(splits[0], "MM-dd-yyyy HH:mm:ss", null);
+                data[j] = splits[1];
+            }
+            string json = JsonConvert.SerializeObject(dateTimes);
+            string json2 = JsonConvert.SerializeObject(data);
+            json = json.Substring(1);
+            json2 = json2.Substring(1);
+            int lastDirSep = file.LastIndexOf(Path.DirectorySeparatorChar);
+            int length = file.Length - 5 - lastDirSep;
+            json = "[\"\"," + json;
+            json2 = "[\"" + file.Substring(lastDirSep + 1, length) + "\"," + json2;
+            File.WriteAllText(file.Substring(0, file.Length - 4) + ".json", "{ \"dates\": " + json + ", \"vals\": " + json2 + "}");
         }
     }
 }
