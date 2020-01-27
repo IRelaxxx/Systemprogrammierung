dc2510ebd3002def94ddad80931055da867d2644
Alexander Krahl <alexander.krahl@stud.htwk-leipzig.de>
2020-01-27 11:38:29 +0100

    catch exception

--- Datalogger/GPIOModule.cs
+++ Datalogger/GPIOModule.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 
 namespace Datalogger
@@ -26,10 +27,16 @@ namespace Datalogger
 
         public void setPWMValue(byte val)
         {
-            using (var dev = File.OpenWrite(drvPath))
+            try
             {
-                dev.WriteByte(val);
-            }
+                using (var dev = File.OpenWrite(drvPath))
+                {
+                    dev.WriteByte(val);
+                }
+            } catch(IOException ex)
+            {
+                Console.WriteLine("Could not open(write) device driver");
+            } 
         }
     }
 }
